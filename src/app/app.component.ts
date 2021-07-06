d4db336b9548db05cd5d848a4c213f1ed9e9138b
Mangesh Kumar <mangesh2599@gmail.com>
2021-07-06 10:16:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+//import { FormControl, FormGroup } from '@angular/forms'
+import {FormBuilder, Validators} from '@angular/forms'
+import { PasswordValidator } from './shared/password.validator';
+import { ForbiddenNameValidator } from './shared/username.validator';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rdf';
+  constructor(private fb:FormBuilder){
+
+  }
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Mangesh'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl(''),
+  //   })
+  // })
+  registrationForm=this.fb.group({
+    userName:['',[Validators.required,Validators.minLength(3),ForbiddenNameValidator]],
+    password:[''],
+    confirmPassword:[''],
+    address:this.fb.group({
+      city:[''],
+      state:[''],
+      postalCode:['']
+    })
+  },{validator: PasswordValidator})
+  loadApiData() {
+    this.registrationForm.setValue({
+      userName: 'Mangesh',
+      password: 'test',
+      confirmPassword: 'test',
+      address: {
+        city: 'Patna',
+        state: 'Bihar',
+        postalCode: '9898',
+      }
+    })
+  }
+  onSubmit(){
+    console.log(this.registrationForm.value)
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClient} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms'
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
